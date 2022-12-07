09bdd0633b56c1c90da6aef569ae828a678568a2
tgrsnts <tegarsantoso@gmail.com>
2022-12-07 22:43:07 +0700

    menyesuaikan footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="copyright text-center text-white pt-4">
+            <div className="copyright text-center text-white p-3">
                 &copy; Copyright <strong><span>Tegar</span></strong>. All Rights Reserved
             </div>
         </div>
